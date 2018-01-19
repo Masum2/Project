89b413c4477f3b0def0cc6e9659269aed88cf10a
Masum2 <32471729+Masum2@users.noreply.github.com>
2018-01-20 00:41:54 +0600

    Create form.php

---
+++ form.php
@@ -0,0 +1,39 @@
+<html>
+<head><title>Admin Panel</title>
+<style>
+*{margin: 0;padding: 0;outline: 0;}
+body{
+   margin: 0px;
+}
+.main {
+   width: 250px;
+   height: 151px;
+   border: 1px solid silver;
+   margin: 0 auto;
+   margin: 0 auto;
+   margin-top: 40px;
+   padding-top: 10px;
+   padding-left: 20px;
+   border-radius: 8px 8px 8px 8px;
+   background-color: green;
+   color: white;
+   margin-top: 135px;
+}
+.submit {
+   margin-left: 60px;
+   margin-top: 10px;
+}
+</style>
+</head>
+<body>
+<div class="main">
+   <form>
+   Email:<input type="text" placeholder="Email"><br><br>
+   Password:<input type="text" placeholder="password"><br><br>
+   <div class="submit"><input type="submit" style="width:105px;" value="Login">
+   </div>
+   </form>
+
+</div>
+</body>
+</html>
